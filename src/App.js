2006d82060fbac0155387b8038161b7cbf43500d
nnanhtudev <nnanhtu.dev@gmail.com>
2023-11-04 09:49:03 +0700

    display Login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-// import logo from "./logo.svg";
-// import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Navigation/Nav";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello word</p>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Nav /> {/* Render the Nav component outside of the Switch */}
+        <Switch>
+          <Route exact path="/">
+            Home
+          </Route>
+          <Route path="/news"></Route>
+          <Route path="/contact"></Route>
+          <Route path="/about"></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="*">404 not found</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
